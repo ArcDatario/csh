7d1c80a093ee28f7059477ff84fe74680165b3d7
ArcDatario <91425235+ArcDatario@users.noreply.github.com>
2025-10-01 01:23:56 +0800

    fixing the footer

--- user/footer.php
+++ user/footer.php
@@ -1,18 +1,39 @@
 <!-- Footer inside main content -->
 <footer class="quotes-main-footer">
-    <div class="quotes-footer-simple">
-        <div class="quotes-footer-content">
-            <h3>CSH Enterprises</h3>
-            <p>Innovative cloth printing solutions for businesses, organizations, and individuals.</p>
-            <div class="quotes-social-links">
-                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                <a href="#"><i class="fab fa-instagram"></i></a>
-                <a href="#"><i class="fab fa-twitter"></i></a>
-                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+    <div class="footer-container">
+            <div class="footer-col">
+                <h3>CSH Enterprises</h3>
+                <p>Innovative cloth printing solutions for businesses, organizations, and individuals.</p>
+            </div>
+            <div class="footer-col">
+                <h3>Quick Links</h3>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#gallery">Gallery</a></li>
+                    <li><a href="#contact">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h3>Services</h3>
+                <ul>
+                    <li><a href="#">Screen Printing</a></li>
+                    <li><a href="#">DTG Printing</a></li>
+                    <li><a href="#">Sublimation</a></li>
+                    <li><a href="#">Embroidery</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h3>Connect</h3>
+                <div class="social-links">
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                </div>
             </div>
         </div>
-        <div class="quotes-copyright">
+        <div class="copyright">
             <p>&copy; 2025 CSH Enterprises. All Rights Reserved.</p>
         </div>
-    </div>
 </footer>
